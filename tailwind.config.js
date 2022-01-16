b9df8dd0a2277704d4e1b52243d96af4d367eda3
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-01-16 18:43:06 -0500

    Tailwind defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+	theme: {
+		extend: {
+            colors: {
+                background: "#F0EFE8",
+                secondary: "#E6E5DB",
+                lightGray: "#A6A39B",
+                medGray: "#63625F",
+                darkGray: "#444340",
+                black: "#181818",
+                green: "#97AA6E",
+                overlay: "#5F5F5D66"
+            },
+            fontFamily: {
+                sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+                serif: ["Eczar", ...defaultTheme.fontFamily.serif],
+            }
+        },
+	},
+	plugins: [],
+};
