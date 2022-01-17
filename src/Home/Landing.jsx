3e7efce21e15bf68e00a344a181d88eee7a7e07f
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-01-17 11:41:37 -0500

    Landing.jsx

---
+++ src/Home/Landing.jsx
@@ -0,0 +1,22 @@
+import landing from "../assets/landing.jpeg";
+
+const Landing = () => {
+    return (
+        <div className="space-y-4">
+            <h1 className="font-serif text-darkGray text-4xl font-bold text-center">
+                Making dream homes dreamier.
+            </h1>
+            <p className="text-medGray text-lg text-center">At Kevin's Bazaar, we provide our customers with premium, custom made furnishing for their homes.</p>
+            <div className="flex justify-center pt-1 pb-3">
+                <a // TODO: ROUTE TO CATALOG
+                    className="px-8 py-3.5 rounded-lg inline-block 
+                    bg-green text-background font-medium">
+                    Shop Now
+                </a>
+            </div>
+            <img className="rounded-xl" src={landing} />
+        </div>
+    )
+}
+
+export default Landing;
