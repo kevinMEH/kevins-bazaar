c4cbf0dbdc3c6cb8bb0ed037742d03f553be669e
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-01-17 20:45:18 -0500

    Footer styling changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 const Footer = () => {
     return (
-        <footer className="text-lightGray space-y-2.5 sm:flex sm:justify-between">
+        <footer className="text-lightGray space-y-2.5 sm:space-y-0 sm:flex sm:justify-between">
             <div className="flex justify-between">
                 <a href="mailto:bazaar@liao.gg">Contact</a>
                 <a href="https://github.com/kevinMEH/kevins-bazaar">Github</a>
                 <a href="https://github.com/kevinMEH/">kevinMEH</a>
             </div>
-            <div>
+            <div className="text-center">
                 kevinMEH © 2022
             </div>
         </footer>
