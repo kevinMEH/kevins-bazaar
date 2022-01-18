baa7057da1e6da002fdc17d1eb4e56fd1e037752
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-01-17 20:13:32 -0500

    Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <footer className="text-lightGray space-y-2.5 sm:flex sm:justify-between">
+            <div className="flex justify-between">
+                <a href="mailto:bazaar@liao.gg">Contact</a>
+                <a href="https://github.com/kevinMEH/kevins-bazaar">Github</a>
+                <a href="https://github.com/kevinMEH/">kevinMEH</a>
+            </div>
+            <div>
+                kevinMEH © 2022
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
