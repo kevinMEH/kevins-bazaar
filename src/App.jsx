0a81962adf3140715d3c1e3ff546abc76e055445
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-01-19 12:17:10 -0500

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 import Home from "./Home/Home";
+import Catalog from "./Catalog/Catalog";
 
-function App() {
-
-	return (
-		<>
-			<Home />
-		</>
-	);
+const App = () => {
+    return (
+            <Routes>
+                <Route path="/" element={ <Home/> } />
+                <Route path="catalog" element={ <Catalog/> } />
+                {/* <Route path="*" element={} */}
+            </Routes>
+    )
 }
 
 
