4b0950ef136db781a36eda354047cc94fac89f6e
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-01-20 19:58:50 -0500

    Header unused styling removed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
                 <h1 className="font-serif text-xl w-20 leading-none font-bold text-lightGray">Kevin's Bazaar</h1>
             </Link>
             <Link to="cart" className="flex items-center">
-                <img src={shoppingCart} className="fill-lightGray h-7" alt="Shopping Cart"/>
+                <img src={shoppingCart} className="h-7" alt="Shopping Cart"/>
             </Link>
         </nav>
     )
