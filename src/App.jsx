58d07e88ed416c220d6efd37384999d397ae8709
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-01-24 20:23:15 -0500

    Basic cart system, localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
@@ -6,18 +8,59 @@ import Footer from "./components/Footer";
 import Home from "./Home/Home";
 import Catalog from "./Catalog/Catalog";
 import ProductPage from "./ProductPage/ProductPage";
+import Cart from "./Cart/Cart";
 import Error from "./Error/Error";
 
+import { validProduct } from "./Data";
+
 const App = () => {
+    const [ cart, setCart ] = useState(null);
+    
+    function addToCart(product) {
+        if(validProduct(product)) setCart([...cart, product]);
+    }
+    
+    function removeFromCart(product) {
+        if(inCart(product)) {
+            let newCart = [...cart];
+            newCart.splice(newCart.findIndex(item => item.url === product.url), 1)
+            setCart(newCart);
+        }
+    }
+    
+    useEffect(() => {
+        console.log("usedEffect");
+        let localCart = localStorage.getItem("cart");
+        let parsedCart = JSON.parse(localCart); // cart: Array<Product>
+        if(parsedCart) {
+            parsedCart = parsedCart.filter(product => validProduct(product));
+            setCart(parsedCart);
+        } else setCart([]);
+    }, []);
+
+        
+    function inCart(product) {
+        // .includes() does not work, so matching URL
+        for(let item of cart) {
+            if(item.url === product.url) return true;
+        }
+        return false;
+    }
+    
+    useEffect(() => {
+        if(cart) localStorage.setItem("cart", JSON.stringify(cart));
+    }, [cart]);
+
     return (
         <div className="p-6 min-h-screen flex flex-col justify-between">
             <Header />
-            <Routes>
-                <Route path="/" element={ <Home/> } />
-                <Route path="catalog" element={ <Catalog/> } />
-                <Route path="catalog/:productURL" element={<ProductPage />} />
-                <Route path="*" element={ <Error /> } />
-            </Routes>
+                <Routes>
+                    <Route path="/" element={ <Home /> } />
+                    <Route path="catalog" element={ <Catalog addToCart={addToCart} inCart={inCart} /> } />
+                    <Route path="catalog/:productURL" element={<ProductPage addToCart={addToCart} inCart={inCart} cart={cart} />} />
+                    <Route path="cart" element={ <Cart cart={cart} removeFromCart={removeFromCart} /> } />
+                    <Route path="*" element={ <Error /> } />
+                </Routes>
             <Footer />
         </div>
     )
