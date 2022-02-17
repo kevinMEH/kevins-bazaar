cd0705b2f22310dd42930bb9db1d2c1925745774
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-02-17 12:04:49 -0500

    Forces motto to break and not break Forces the motto to break in the center and prevents breaking anywhere else.

--- src/Home/Landing.jsx
+++ src/Home/Landing.jsx
@@ -8,10 +8,10 @@ const Landing = () => {
             </div>
 
             <div className="mt-9 md:mt-0 space-y-4 text-center md:text-left">
-                <h1 className="font-serif text-darkGray text-4xl md:text-[2.75rem] lg:text-6xl font-bold">
-                    Making dream homes dreamier.
+                <h1 className="inline-block whitespace-nowrap font-serif text-darkGray text-4xl md:text-[2.75rem] lg:text-6xl font-bold">
+                    Making dream <br /> homes dreamier.
                 </h1>
-                <p className="text-medGray text-lg lg:text-xl">At Kevin's Bazaar, we provide our customers with premium, custom made furnishing for their homes.</p>
+                <p className="text-medGray text-lg lg:text-xl lg:mr-8">At Kevin's Bazaar, we provide our customers with premium, custom made furnishing for their homes.</p>
                 <div className="flex justify-center md:justify-start pt-1">
                     <Link
                         to="catalog"
