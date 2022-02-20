b913e02e5c8981e88ef200caa812e8764aa41d11
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-02-19 20:18:57 -0500

    Cart page complete redesign + md lg support

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -31,9 +31,9 @@ const Cart = ({ cart, removeFromCart }) => {
     }, [cart])
 
     return (
-        <div className="pt-16 pb-24 flex flex-col items-center space-y-12">
-            <h1 className="text-medGray font-bold text-3xl">Your Cart</h1>
-            <div className="space-y-5">
+        <div className="pt-16 lg:pt-20 pb-24 lg:pb-28 flex-1 w-full justify-between flex flex-col gap-12 lg:gap-14 items-center max-w-4xl">
+            <h1 className="text-medGray font-bold text-3xl lg:text-4xl">Your Cart</h1>
+            <div className="space-y-5 md:space-y-8 lg:space-y-9 flex-1">
                 {items}
             </div>
             <button className="px-5 py-3.5 bg-green text-background rounded-lg font-medium flex space-x-2 items-center"><p>Check Out</p><img src={arrowRight} className="inline h-5" /></button>
@@ -49,30 +49,27 @@ Cart.propTypes = {
 const Item = ({ product, removeFromCart }) => {
     const navigate = useNavigate();
 
-    function navigateToProductPage() {
-        navigate("../catalog/" + product.url, {state: {product}});
-    }
-
     function handleRFC(event) {
         event.stopPropagation();
         removeFromCart(product.url);
     }
-
+    
     return (
-        <a onClick={navigateToProductPage} className="block md:hover:bg-hover -m-2.5 p-4 rounded-xl">
-            <img className="rounded-lg h-52 w-full object-cover" src={product.thumbnail} />
-            <div className="flex justify-between pt-3 px-1 pb-1">
-                <div className="text-medGray">
-                    <h2 className="font-semibold text-lg leading-snug">{product.name}</h2>
-                    <p className="leading-relaxed">{product.price}</p>
+        <a onClick={() => navigate("/catalog/" + product.url)}
+            className="grid grid-cols-9 sm:grid-cols-4 gap-3 sm:gap-6 lg:gap-8"
+        >
+            <img src={product.thumbnail} className="col-span-3 sm:col-span-1 rounded-md md:rounded-lg w-full h-auto object-cover aspect-4/3 cursor-pointer" />
+            <div className="col-span-6 sm:col-span-3 flex justify-between gap-2">
+                <div className="text-medGray mt-1 md:mt-2">
+                    <h2 className="font-semibold leading-tight sm:text-lg md:text-xl lg:text-2xl">{product.name}</h2>
+                    <p className="leading-relaxed text-sm sm:text-base lg:text-lg">{product.price}</p>
                 </div>
-                
-                <div onClick={handleRFC} className="flex">
-                    <img src={trash} className="h-7 mt-1 pr-1" />
+                <div onClick={handleRFC} className="col-span-1 flex items-center">
+                    <img src={trash} className="h-7 md:h-8 lg:h-9 cursor-pointer" />
                 </div>
             </div>
         </a>
-    )
+    );
 }
 
 Item.propTypes = {
