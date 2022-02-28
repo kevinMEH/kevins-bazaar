06bf4605eca1a7df3d82c580b21789f62d09f661
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-02-28 11:24:28 -0500

    Fetches from api.bazaar.liao.gg

--- src/Data.jsx
+++ src/Data.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 function getPage(page = 1, filter = "") {
     return new Promise((resolve) => {
         let params = filter ? { page, filter } : { page };
-        axios.get("http://localhost:80/api/products", { params }).then( response => {
+        axios.get("https://api.bazaar.liao.gg/api/products", { params }).then( response => {
             resolve({ error: null, products: response.data });
         }).catch( error => {
             resolve({ error, products: null });
@@ -13,7 +13,7 @@ function getPage(page = 1, filter = "") {
 
 function getProduct(productURL) {
     return new Promise((resolve) => {
-        axios.get("http://localhost:80/api/product/" + productURL).then( response => {
+        axios.get("https://api.bazaar.liao.gg/api/product/" + productURL).then( response => {
             if(!response.data) console.log("Note: " + productURL + " does not exist.");
             resolve({ error: null, product: response.data, url: productURL });
         }).catch( error => {
