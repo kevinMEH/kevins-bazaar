520a96dff544d8716ec22b7e3b83d6f8daad7bce
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2022-02-28 21:19:37 -0500

    Delayed search to avoid spamming API If user continues typing during the 250 ms after the initial keystroke, the old filter change will be ignored and replaced with a new one. If there is no activity for 250 ms, then the filter will be changed.

--- src/Catalog/Search.jsx
+++ src/Catalog/Search.jsx
@@ -1,14 +1,24 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 import menu from "../assets/menu.svg";
 import searchIcon from "../assets/search.svg";
 
 const Search = ({ filter, setFilter }) => {
     
+    const [searchTerm, setSearchTerm] = useState(filter);
+    const [searchTimeout, setSearchTimeout] = useState(null);
+    
+    function handleChange(event) {
+        if(searchTimeout) clearTimeout(searchTimeout);
+        setSearchTerm(event.target.value);
+        setSearchTimeout(setTimeout(() => setFilter(event.target.value), 250));
+    }
+    
     return (
         <div className="relative border-[1.75px] border-lightGray rounded-md lg:rounded-lg mx-auto">
             <img src={menu} className="absolute left-3.5 top-1/2 -translate-y-1/2 lg:h-7 lg:left-5" />
-            <input type="text" value={filter} onChange={event => setFilter(event.target.value)} 
+            <input type="text" value={searchTerm} onChange={handleChange} 
                 className="bg-transparent py-2.5 lg:py-3.5 px-14 lg:px-16 w-full
                 font-medium text-medGray rounded-[4.5px] lg:text-lg
                 focus:outline-none focus:ring-[1.75px] focus:ring-medGray"
